bf27ab8c03877a68552af7e6de3c447f86a92335
Vilson Vieira <vilson@void.cc>
2016-06-17 04:28:25 -0400

    Tabify it

--- test/wayback.js
+++ test/wayback.js
@@ -2,41 +2,41 @@
 'use strict';
 
 var assert = require('assert'),
-    fs = require('fs'),
-    wayback = require('../lib/wayback');
+		fs = require('fs'),
+		wayback = require('../lib/wayback');
 
 var url = 'http://automata.cc/thumb-chuck-wiimote.png';
 
 describe('wayback', function() {
-  describe('getClosest', function() {
-    it('should get the closest url on archive', function(done) {
-      wayback.getClosest(url, function(err, resp) {
-        assert.ok(err === null);
-        assert.ok(resp instanceof Object);
-        assert.equal(resp.available, true);
-        done();
-      });
-    });
-    it('should get the closest url while using options', function(done) {
-      this.timeout(6000);
-      var options = {'timeout': 5000};
-      wayback.getClosest(url, options, function(err, resp) {
-        assert.ok(err === null);
-        assert.ok(resp instanceof Object);
-        assert.equal(resp.available, true);
-        done();
-      });
-    });
-    it('should throw an error on timeout', function(done) {
-      // Force a timeout
-      this.timeout(10000);
-      var options = {'timeout': 100};
-      wayback.getClosest(url, options, function(err, resp) {
-        assert.ok(err !== null);
-        assert.ok(err instanceof Error);
-        assert.ok(resp === null);
-        done();
-      });
-    });
-  });
+	describe('getClosest', function() {
+		it('should get the closest url on archive', function(done) {
+			wayback.getClosest(url, function(err, resp) {
+				assert.ok(err === null);
+				assert.ok(resp instanceof Object);
+				assert.equal(resp.available, true);
+				done();
+			});
+		});
+		it('should get the closest url while using options', function(done) {
+			this.timeout(6000);
+			var options = {'timeout': 5000};
+			wayback.getClosest(url, options, function(err, resp) {
+				assert.ok(err === null);
+				assert.ok(resp instanceof Object);
+				assert.equal(resp.available, true);
+				done();
+			});
+		});
+		it('should throw an error on timeout', function(done) {
+			// Force a timeout
+			this.timeout(10000);
+			var options = {'timeout': 100};
+			wayback.getClosest(url, options, function(err, resp) {
+				assert.ok(err !== null);
+				assert.ok(err instanceof Error);
+				assert.ok(resp === null);
+				done();
+			});
+		});
+	});
 });
